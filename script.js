7bde378fee469f222c0b4ebd094753c2d009668d
Brendo <brendowashingtone@gmail.com>
2024-09-29 23:49:11 -0300

    Terminei
    Terminei

--- script.js
+++ script.js
@@ -19,9 +19,6 @@ function verificarIdade() {
   divs.forEach((div) => {
     verificarInput(div);
   });
-
-
-  
 }
 
 function verificarInput(div) {
@@ -29,40 +26,58 @@ function verificarInput(div) {
   const span = div.querySelector("span");
   const label = div.querySelector("label");
 
-  if (input.value == "") {
+  if (ano.value > anoAtual) {
     input.style.border = "2px solid red";
     span.style.display = "block";
     label.style.color = "red";
-    resultadoDia.innerHTML = "--";
-    resultadoMes.innerHTML = "--";
-    resultadoAno.innerHTML = "--";
+  } else if (ano.value >= anoAtual && mes.value > mesAtual + 1) {
+    input.style.border = "2px solid red";
+    span.style.display = "block";
+    label.style.color = "red";
+  } else if (dia.value > 31 || mes.value > 12) {
+    input.style.border = "2px solid red";
+    span.style.display = "block";
+    label.style.color = "red";
   } else {
-    input.style.border = "1px solid rgb(173, 171, 171)";
-    span.style.display = "none";
-    label.style.color = "black";
-
-    let mesAno = anoAtual - ano.value;
-
-  if (dia.value == "" || mes.value == "" || ano.value == "") {
-    resultadoDia.innerHTML = "--";
-    resultadoMes.innerHTML = "--";
-    resultadoAno.innerHTML = "--";
-  } else {
-    if (dia.value < diaAtual) {
-      resultadoMes.innerHTML = mesAtual - mes.value + 1;
+    if (input.value == "") {
+      input.style.border = "2px solid red";
+      span.style.display = "block";
+      label.style.color = "red";
+      resultadoDia.innerHTML = "--";
+      resultadoMes.innerHTML = "--";
+      resultadoAno.innerHTML = "--";
     } else {
-      resultadoMes.innerHTML = mesAtual - mes.value;
-    }
+      input.style.border = "1px solid rgb(173, 171, 171)";
+      span.style.display = "none";
+      label.style.color = "black";
 
-    if (dia.value > diaAtual) {
-      const diaN = Number(dia.value);
-      let total = diaN - diaAtual;
-      resultadoDia.innerHTML = 30 - total;
-    } else {
-      resultadoDia.innerHTML = diaAtual - Number(dia.value);
-    }
+      let mesAno = anoAtual - ano.value;
 
-    resultadoAno.innerHTML = mesAno;
-  }
+      if (dia.value == "" || mes.value == "" || ano.value == "") {
+        resultadoDia.innerHTML = "--";
+        resultadoMes.innerHTML = "--";
+        resultadoAno.innerHTML = "--";
+      } else {
+        if (mes.value <= mesAtual + 1) {
+          resultadoMes.innerHTML = mesAtual + 1 - mes.value;
+        } else {
+          let temp = mes.value - (mesAtual + 1);
+          resultadoMes.innerHTML = 12 - temp;
+          console.log(mes.value);
+          console.log(mesAtual);
+          console.log(temp);
+        }
+
+        if (dia.value > diaAtual) {
+          const diaN = Number(dia.value);
+          let total = diaN - diaAtual;
+          resultadoDia.innerHTML = 30 - total;
+        } else {
+          resultadoDia.innerHTML = diaAtual - Number(dia.value);
+        }
+
+        resultadoAno.innerHTML = mesAno;
+      }
+    }
   }
 }
